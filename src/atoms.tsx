5b1dd1c056bb4f55c7846380af6953af4278fcb9
wwwkimminja <82824976+wwwkimminja@users.noreply.github.com>
2025-06-17 21:48:21 +0900

    Add category state and selector for filtering ToDos in Recoil

--- src/atoms.tsx
+++ src/atoms.tsx
@@ -1,12 +1,26 @@
-import { atom } from "recoil";
+import { atom ,selector} from "recoil";
 
 export interface IToDo {
   text: string;
   id: number;
   category: "TO_DO" | "DOING" | "DONE";
 }
+export const categoryState = atom<"TO_DO" | "DOING" | "DONE">({
+  key: 'category',
+  default: 'TO_DO',
+});
+
 
 export const toDoState = atom<IToDo[]>({
   key: "toDo",
   default: [],
+});
+
+export const toDoSelector = selector({
+  key: 'toDoSelector',
+  get: ({ get }) => {
+    const toDos = get(toDoState);
+    const category = get(categoryState);
+    return toDos.filter((todo) => todo.category === category);
+  },
 });
